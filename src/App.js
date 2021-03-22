fc3ea18ad01464b010973cc8c0d6f163aa9dc240
Barkley Santo <barkley.santo@gmail.com>
2021-03-22 14:13:12 -0600

    create search input with classname and placeholder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ const API = {
 function App() {
   return (
     <div className="App">
-
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
