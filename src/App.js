5fe7b9e60a14620cca6f1a4ed9043d49acbdac10
Barkley Santo <barkley.santo@gmail.com>
2021-03-22 14:07:20 -0600

    create api object with key and base url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+const API = {
+  key: '364545f834bbb6e7f867173e9f886040',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
     <div className="App">
-      hi
+
     </div>
   );
 }
