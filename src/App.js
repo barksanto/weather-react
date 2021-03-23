e1e8191395c3397ecc7b5b944ee80006b2910d92
Barkley Santo <barkley.santo@gmail.com>
2021-03-23 10:57:31 -0600

    create search function to fetch from weather api using user search query and APPID key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
+
 const API = {
   key: '364545f834bbb6e7f867173e9f886040',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = event => {
+    if(event.key === "Enter"){
+      fetch(`${API.base}weather?q=${query}&units=metric&APPID=${API.key}`)
+        .then(result => result.json)
+        .then(res => setWeather(res));
+    }
+  }
+
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
